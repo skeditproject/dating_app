28817e7fb2eeac65033ccc675ff10ba0ce5f4b2b
skeditproject <skeditproject@gmail.com>
2024-12-17 16:36:09 +0530

    correct class name

--- app/serializers/messages_serializer.rb
+++ app/serializers/messages_serializer.rb
@@ -1,3 +1,3 @@
-class MessageSerializer < ActiveModel::Serializer
+class MessagesSerializer < ActiveModel::Serializer
   attributes :id, :sender_id, :recipient_id, :content, :created_at, :pair_id
 end
